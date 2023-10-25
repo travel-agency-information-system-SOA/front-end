de8cb36614f07f02bd69831a4aae071a4aef6edf
Ninoli1 <116906127+Ninoli1@users.noreply.github.com>
2023-10-25 23:29:06 +0200

    feat: CRUD for TourReview

--- Explorer/src/app/feature-modules/marketplace/marketplace.module.ts
+++ Explorer/src/app/feature-modules/marketplace/marketplace.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TourReviewFormComponent } from './tour-review-form/tour-review-form.component';
+import { MaterialModule } from 'src/app/infrastructure/material/material.module';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TourReviewsShowComponent } from './tour-reviews-show/tour-reviews-show.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TourReviewFormComponent,
+    TourReviewsShowComponent,
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class MarketplaceModule { }
--- Explorer/src/app/infrastructure/routing/app-routing.module.ts
+++ Explorer/src/app/infrastructure/routing/app-routing.module.ts
@@ -5,12 +5,17 @@ import { LoginComponent } from '../auth/login/login.component';
 import { EquipmentComponent } from 'src/app/feature-modules/administration/equipment/equipment.component';
 import { AuthGuard } from '../auth/auth.guard';
 import { RegistrationComponent } from '../auth/registration/registration.component';
+import { TourReviewFormComponent } from 'src/app/feature-modules/marketplace/tour-review-form/tour-review-form.component';
+import { TourReviewsShowComponent } from 'src/app/feature-modules/marketplace/tour-reviews-show/tour-reviews-show.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegistrationComponent},
-  {path: 'equipment', component: EquipmentComponent, canActivate: [AuthGuard],}
+  {path: 'equipment', component: EquipmentComponent, canActivate: [AuthGuard],},
+  {path: 'tourReviewForm', component: TourReviewFormComponent},
+  {path:'tourReviewShow', component: TourReviewsShowComponent}
+
 ];
 
 @NgModule({
