7a69cc05d22b052e7b21f7ebdbf500eb7e63377e
anabb55 <75089113+anabb55@users.noreply.github.com>
2023-10-26 11:12:31 +0200

    Resolved conflicts

--- Explorer/src/app/infrastructure/routing/app-routing.module.ts
+++ Explorer/src/app/infrastructure/routing/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from '../auth/login/login.component';
 import { AuthGuard } from '../auth/auth.guard';
 import { RegistrationComponent } from '../auth/registration/registration.component';
 import { GuideReviewComponent } from 'src/app/feature-modules/marketplace/guide-review/guide-review.component';
-import { PreferencesComponent } from "../../feature-modules/marketplace/preferences/preferences.component";
+import { PreferencesComponent } from '../../feature-modules/marketplace/preferences/preferences.component';
 
 import { TourPointsComponent } from 'src/app/feature-modules/tour-authoring/tour-points/tour-points.component';
 import { TourComponent } from 'src/app/feature-modules/tour-authoring/tour/tour.component';
@@ -25,12 +25,20 @@ const routes: Routes = [
   },
   { path: 'objects', component: TourObjectComponent },
   { path: 'tourMap/:id', component: TourMapComponent },
-  {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegistrationComponent},
-  {path: 'equipment', component: EquipmentComponent, canActivate: [AuthGuard]},
-  {path: 'guideReview', component: GuideReviewComponent, canActivate: [AuthGuard]},
-  {path: 'preferences', component: PreferencesComponent, canActivate: [AuthGuard]}
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+  // {path: 'equipment', component: EquipmentComponent, canActivate: [AuthGuard]},
+  {
+    path: 'guideReview',
+    component: GuideReviewComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'preferences',
+    component: PreferencesComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
