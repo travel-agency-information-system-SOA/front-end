6f23bb2c591ed04632650b3912b3e5132a77a397
ninak235 <116868070+ninak235@users.noreply.github.com>
2023-11-12 15:36:46 +0100

    Push to Medic

--- Explorer/src/app/feature-modules/marketplace/problem/problem.component.ts
+++ Explorer/src/app/feature-modules/marketplace/problem/problem.component.ts
@@ -85,9 +85,9 @@ export class ProblemComponent implements OnInit {
   
   private isSameDate(date1: Date, date2: Date): boolean {
     return (
-      date1.getFullYear() === date2.getFullYear() &&
-      date1.getMonth() === date2.getMonth() &&
-      date1.getDate() === date2.getDate()
+      date1.getFullYear() >= date2.getFullYear() &&
+      date1.getMonth() >= date2.getMonth() &&
+      date1.getDate() >= date2.getDate()
     );
   }
 
