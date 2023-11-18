16207fdb2c6263b268e158d6971122d1401aaf53
lazarristic023 <116806166+lazarristic023@users.noreply.github.com>
2023-11-18 20:57:43 +0100

    finished showing all equipment

--- Explorer/src/app/feature-modules/marketplace/marketplace.service.ts
+++ Explorer/src/app/feature-modules/marketplace/marketplace.service.ts
@@ -331,6 +331,10 @@ export class MarketplaceService {
     return this.http.get<Tour>('https://localhost:44333/api/marketplace/' + id);
   } 
 
+  getSelectedTourr(id: number): Observable<Tour> {
+    return this.http.get<Tour>('https://localhost:44333/api/marketplace/selectedTour/' + id);
+  } 
+
   getShoppingCart(touristId: number): Observable<ShoppingCart>{
     return this.http.get<ShoppingCart>('https://localhost:44333/api/shoppingcart/'+touristId);
   }
