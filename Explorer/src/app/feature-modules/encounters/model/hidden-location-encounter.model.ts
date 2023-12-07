3e8e460a0780b0e0cb70865555889d0c69f0348a
spasoje2001 <spasoje.brboric@gmail.com>
2023-12-07 06:23:33 +0100

    Changed encounters management page, to support working with different types of encounters and their specific sets of fields

---
+++ Explorer/src/app/feature-modules/encounters/model/hidden-location-encounter.model.ts
@@ -0,0 +1,15 @@
+export interface HiddenLocationEncounter {
+    id: number,
+    name: string,
+    description: string,
+    xpPoints: number,
+    status: string,
+    type: string,
+    latitude: number,
+    longitude: number,
+    encounterId: number,
+    imageURL: string,
+    imageLatitude: number,
+    imageLongitude: number,
+    distanceTreshold: number
+}
---
+++ Explorer/src/app/feature-modules/encounters/model/social-encounter.model.ts
@@ -0,0 +1,14 @@
+export interface SocialEncounter {
+    encounterId: number,
+    name: string,
+    description: string,
+    xpPoints: number,
+    status: string,
+    type: string,
+    latitude: number,
+    longitude: number,
+    id: number,
+    touristsRequiredForCompletion: number,
+    distanceTreshold: number,
+    touristIDs:  number[] | null,
+}
