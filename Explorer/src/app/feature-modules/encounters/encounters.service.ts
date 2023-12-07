d61427bfe50ebd15ebecc730bf5e28d0e15296ad
Zela11 <zelovic.ra106.2020@uns.ac.rs>
2023-12-07 06:08:17 +0100

    Implemented creating encounter from keypoint

--- Explorer/src/app/feature-modules/encounters/encounters.service.ts
+++ Explorer/src/app/feature-modules/encounters/encounters.service.ts
@@ -4,6 +4,8 @@ import { Encounter } from './model/encounter.model';
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
 import { Observable } from 'rxjs';
 import { environment } from 'src/env/environment';
+import { SocialEncounter } from './model/social-encounter.model';
+import { HiddenLocationEncounter } from './model/hidden-location-encounter.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,12 +18,36 @@ export class EncountersService {
     return this.http.get<PagedResults<Encounter>>(environment.apiHost + 'encounters');
   }
 
+  getSocialEncounters(): Observable<PagedResults<SocialEncounter>> {
+    return this.http.get<PagedResults<SocialEncounter>>(environment.apiHost + 'encounters/social');
+  }
+
+  getHiddenLocationEncounters(): Observable<PagedResults<HiddenLocationEncounter>> {
+    return this.http.get<PagedResults<HiddenLocationEncounter>>(environment.apiHost + 'encounters/hiddenLocation');
+  }
+
   addEncounter(encounter: Encounter): Observable<Encounter>{
     return this.http.post<Encounter>(environment.apiHost + 'encounters', encounter);
   }
 
+  addSocialEncounter(encounter: SocialEncounter): Observable<SocialEncounter>{
+    return this.http.post<SocialEncounter>(environment.apiHost + 'encounters/social', encounter);
+  }
+
+  addHiddenLocationEncounter(encounter: HiddenLocationEncounter): Observable<HiddenLocationEncounter>{
+    return this.http.post<HiddenLocationEncounter>(environment.apiHost + 'encounters/hiddenLocation', encounter);
+  }
+
   updateEncounter(encounter: Encounter): Observable<Encounter>{
-    return this.http.put<Encounter>(environment.apiHost + 'encounters/' + encounter.id, encounter);
+    return this.http.put<Encounter>(environment.apiHost + 'encounters', encounter);
+  }
+
+  updateSocialEncounter(encounter: SocialEncounter): Observable<SocialEncounter>{
+    return this.http.put<SocialEncounter>(environment.apiHost + 'encounters/social', encounter);
+  }
+
+  updateHiddenLocationEncounter(encounter: HiddenLocationEncounter): Observable<HiddenLocationEncounter>{
+    return this.http.put<HiddenLocationEncounter>(environment.apiHost + 'encounters/hiddenLocation', encounter);
   }
 
   deleteEncounter(encounter: Encounter): Observable<Encounter>{
