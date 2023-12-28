091ea7246d0e0fcc31d9169ea8ca8ed9baa1cdb3
anabb55 <75089113+anabb55@users.noreply.github.com>
2023-12-28 15:35:08 +0100

    Change UI experience for tour review

--- Explorer/src/app/feature-modules/marketplace/update-review/update-review.component.ts
+++ Explorer/src/app/feature-modules/marketplace/update-review/update-review.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { MarketplaceService } from '../marketplace.service';
 import { TourReview } from '../model/tourReview.model';
@@ -8,47 +16,42 @@ import { User } from 'src/app/infrastructure/auth/model/user.model';
 @Component({
   selector: 'xp-update-review',
   templateUrl: './update-review.component.html',
-  styleUrls: ['./update-review.component.css']
+  styleUrls: ['./update-review.component.css'],
 })
 export class UpdateReviewComponent implements OnChanges {
-
-
-  
-  
   @Output() addedObject = new EventEmitter<null>();
 
-  showComponent:boolean=true;
-  id: number;
-  @Input()reviewForUpdate : TourReview;
+  showComponent: boolean = true;
+  id: number | undefined;
+  @Input() reviewForUpdate: TourReview;
 
   url: string;
-  inputForm= new FormGroup({
-    grade: new FormControl('',[Validators.required]),
-    comment: new FormControl('',[Validators.required]),
-    images: new FormControl('',[Validators.required])
-  })
+  inputForm = new FormGroup({
+    grade: new FormControl('', [Validators.required]),
+    comment: new FormControl('', [Validators.required]),
+    images: new FormControl('', [Validators.required]),
+  });
   img: string;
-  tourReview: TourReview={
+  tourReview: TourReview = {
     id: 0,
     grade: 0,
-    comment:'',
+    comment: '',
     attendanceDate: new Date(),
     reviewDate: new Date(),
     images: [''],
     touristId: 10,
     tourId: 0,
-  
-  }
-  
-  loggedInUser: User={
-    id:0,
-    username:'',
-    role: ''
-  }
+  };
 
-  ngOnChanges(changes: SimpleChanges){
-    console.log("Primlljeni objekat", this.reviewForUpdate.id);
-    this.id=this.reviewForUpdate.id;
+  loggedInUser: User = {
+    id: 0,
+    username: '',
+    role: '',
+  };
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Primlljeni objekat', this.reviewForUpdate.id);
+    this.id = this.reviewForUpdate.id;
     this.inputForm.patchValue({
       grade: this.reviewForUpdate.grade.toString(),
       comment: this.reviewForUpdate.comment,
@@ -56,47 +59,44 @@ export class UpdateReviewComponent implements OnChanges {
 
     console.log(this.inputForm.value.comment);
   }
-  constructor(private authService: AuthService, private tourReviewService: MarketplaceService, ){
-   this.getLoggedInUser();
- 
+  constructor(
+    private authService: AuthService,
+    private tourReviewService: MarketplaceService
+  ) {
+    this.getLoggedInUser();
   }
-  
-  
-    
-  getLoggedInUser(){
-    this.authService.user$.subscribe(user=>{
-      if(user){
-        this.loggedInUser.id=user.id;
-        this.loggedInUser.username=user.username;
-        this.loggedInUser.role=user.role
-        console.log("Ulogovani korisnik", this.loggedInUser);
+
+  getLoggedInUser() {
+    this.authService.user$.subscribe((user) => {
+      if (user) {
+        this.loggedInUser.id = user.id;
+        this.loggedInUser.username = user.username;
+        this.loggedInUser.role = user.role;
+        console.log('Ulogovani korisnik', this.loggedInUser);
       }
-    })
+    });
   }
 
-  
-
-  onEdit(){
-    this.reviewForUpdate.id=this.id;
-    console.log("Id na kraju", this.reviewForUpdate.id);
-    this.reviewForUpdate.grade=parseFloat(this.inputForm.value.grade || '0');
-    this.reviewForUpdate.comment= this.inputForm.value.comment as string;
-    console.log("Editovani objekat", this.reviewForUpdate);
+  onEdit() {
+    this.reviewForUpdate.id = this.id;
+    console.log('Id na kraju', this.reviewForUpdate.id);
+    this.reviewForUpdate.grade = parseFloat(this.inputForm.value.grade || '0');
+    this.reviewForUpdate.comment = this.inputForm.value.comment as string;
+    console.log('Editovani objekat', this.reviewForUpdate);
     this.update(this.reviewForUpdate);
     this.inputForm.reset();
 
-    this.showComponent=false;
-
+    this.showComponent = false;
   }
 
-  update(review: TourReview){
+  update(review: TourReview) {
     this.tourReviewService.updateReview(review).subscribe({
-      next: (response)=>{
+      next: (response) => {
         console.log(response);
       },
-      error: (error)=>{
+      error: (error) => {
         console.log(error);
-      }
-    })
+      },
+    });
   }
 }
